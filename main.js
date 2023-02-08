c761fee4503306706f40a3152051aa5b1b290942
al-chang <aac6692@gmail.com>
2023-02-08 15:43:21 -0500

    Finish assignment

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-const buttonClicked = () => {};
-
+// Add button click event
 const button = document.querySelector("button");
-button.addEventListener("click", buttonClicked);
+button.addEventListener("click", () => {
+  const line = document.querySelector("#line");
+  if (line.getAttribute("stroke") === "red") {
+    line.setAttribute("stroke", "darkblue");
+  } else {
+    line.setAttribute("stroke", "red");
+  }
+});
+
+// Add circle mouse over event
+const circle = document.querySelector("#circle");
+circle.addEventListener("mouseover", () => {
+  circle.setAttribute("fill", "orange");
+});
+circle.addEventListener("mouseout", () => {
+  circle.setAttribute("fill", "yellow");
+});
